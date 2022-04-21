a8172201c265a652cfbd49939a9624060b52c441
William Correa <wccorreas@gmail.com>
2022-04-21 13:48:37 -0500

    Agregue practica deplatillas literales

---
+++ practicaTemplateLiterals.js
@@ -0,0 +1,13 @@
+code//Interpolación de expresiones
+
+let a = 20
+let b = 15
+
+console.log('Cinco es ' + (a-b) + ' y\nno ' + (2*a-b) + '.');
+
+//Aplicando plantillas literales
+
+let c = 40
+let d = 80
+
+console.log(`Ciento veinte ${c+d} y no ${c-d}.`);
